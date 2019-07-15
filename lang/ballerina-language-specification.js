d06b61370f10c9504bf785d3aeb83aabc589d88f
Madhura Mendis <madhuramendis@gmail.com>
2019-07-15 19:06:22 +0530

    Add theme files (#258)

---
+++ lang/ballerina-language-specification.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    var logo = '<a class="cLogo" href="/">' +
+        ' <img src="http://ballerina.io/img/ballerina-logo.png" alt="Ballerina">' +
+        '</a>' ;
+        
+       
+        $('body').prepend(logo);
+
+        var footer = '<div class="cFooter">' +
+       
+        '</div>' ;
+
+        $('body').append(footer);
+
+
+       });
+
+
+        
+        
+    
